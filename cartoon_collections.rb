3fd3dbd34fc57768049b972b3a7dd2c55d076769
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-27 16:46:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,17 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
-end
+  
+  
+end #end roll_call_dwarves
 
 def summon_captain_planet# code an argument here
-  # Your code here
-end
+  
+  
+end #end summon_captain_planet() 
 
 def long_planeteer_calls# code an argument here
-  # Your code here
-end
+  
+  
+end #end long_planeteer_calls()
 
 def find_the_cheese# code an argument here
   # the array below is here to help
